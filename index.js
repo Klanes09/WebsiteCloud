2ef6afadaaa85955fd21ff2e3a89af8b64b60d01
unknown <azklanxpaul09@gmail.com>
2025-05-04 01:46:23 +0800

    New

---
+++ index.js
@@ -0,0 +1,8 @@
+let username;
+username =window.prompt("Enter your name: ");
+document.getElementById("h1").textContent = "Hello " + username + "! Welcome to this page!";
+
+/*document.getElementById("submit").onclick = function() {
+    username = document.getElementById("name").value;
+    document.getElementById("h2").textContent = "Hello " + username + "! Welcome to this page!";
+} */
