c25933b5e13dc8f87b8861bc5d81a16e0c37ecf7
unknown <azklanxpaul09@gmail.com>
2025-05-04 20:36:27 +0800

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 let username;
 username =window.prompt("Enter your name: ");
-document.getElementById("h1").textContent = "Hello " + username + "! Welcome to this page!";
+while(username ==="" || username === null){
+    username = window.prompt("Please Enter your name:");
+    while(username ==="" || username === null){
+        username = window.prompt("Enter your Fucking name name:");
+        while(username ==="" || username === null){
+            username = window.prompt("Enter your Putanginang name:");
+            while(username ==="" || username === null){
+                username = window.prompt("RAHHHHHHHHHH!!!!!");
+            }
+        }
+    }
+}
+//window.alert("Thanks for entering yourname "+ username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase() + "!");
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+document.getElementById("h1").textContent = "Hello " + username + "! Welcome to simple wether api!";
 
 /*document.getElementById("submit").onclick = function() {
     username = document.getElementById("name").value;
